e8895484afd0f0b8e76faa51a4d532bef24f9a6a
Saifurrahman54 <saifibaba54@gmail.com>
2023-12-11 22:10:49 -0800

    Js-2

---
+++ app.js
@@ -0,0 +1,10 @@
+var myName=("Saif Ur Rahman");
+var age =("25");
+var job=("Certified Mobole Application Development");
+var email=("My email address is saifibaba54@gmail.com");
+var study=(" I am trying to learn from the Book A smarter way to learn JavaScript")
+alert(myName);
+alert(age);
+alert(job);
+alert(email);
+alert(study);
